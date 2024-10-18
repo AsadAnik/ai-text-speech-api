f0183693f0c979ee4df341facae1810e3fb8c59e
asadanik <s.engineer63@gmail.com>
2024-10-18 17:47:49 +0600

    setup: dockerise the entire microservices apps monorepo with rabbitmq

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+
+  // region Connect Auth-Service
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'auth_queue',
+      queueOptions: {
+        durable: false,
+      },
+    }
+  });
+
+  // region Connect User-Service
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'user_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.port ?? 3000);
+  console.log('API Gateway is running on port 3000');
 }
+
 bootstrap();
--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { UserServiceModule } from './user-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  // region User-Service Create
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(UserServiceModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'user_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.listen();
+  console.log('User Service is listening...');
 }
 bootstrap();
